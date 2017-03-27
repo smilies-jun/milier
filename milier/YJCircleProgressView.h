be6f804385b3a1ad5deffc0b993192b5bf59656c
amin <21496416@qq.com>
2017-03-27 16:30:44 +0800

    1

---
+++ milier/YJCircleProgressView.h
@@ -0,0 +1,13 @@
+//
+//  YJCircleProgressView.h
+//  milier
+//
+//  Created by amin on 17/2/23.
+//  Copyright © 2017年 yj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YJCircleProgressView : UIView
+
+@end
---
+++ milier/YJCircleProgressView.m
@@ -0,0 +1,21 @@
+//
+//  YJCircleProgressView.m
+//  milier
+//
+//  Created by amin on 17/2/23.
+//  Copyright © 2017年 yj. All rights reserved.
+//
+
+#import "YJCircleProgressView.h"
+
+@implementation YJCircleProgressView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
