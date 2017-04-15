dc47f951f601027ba14bbda72e604537a93aa61c
amin <21496416@qq.com>
2017-04-16 00:20:04 +0800

    no message

---
+++ milier/ProuctModel.h
@@ -0,0 +1,13 @@
+//
+//  ProuctModel.h
+//  milier
+//
+//  Created by amin on 17/4/15.
+//  Copyright © 2017年 yj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ProuctModel : NSObject
+
+@end
---
+++ milier/ProuctModel.m
@@ -0,0 +1,13 @@
+//
+//  ProuctModel.m
+//  milier
+//
+//  Created by amin on 17/4/15.
+//  Copyright © 2017年 yj. All rights reserved.
+//
+
+#import "ProuctModel.h"
+
+@implementation ProuctModel
+
+@end
