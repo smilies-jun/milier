ae435e12e54b633338186db50eb6a7abdef025bc
amin <21496416@qq.com>
2017-05-16 18:13:40 +0800

    5-16 alert

---
+++ milier/ActivityDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ActivityDetailViewController.h
+//  milier
+//
+//  Created by amin on 2017/5/16.
+//  Copyright © 2017年 yj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActivityDetailViewController : UIViewController
+
+@end
---
+++ milier/ShareDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShareDetailViewController.h
+//  milier
+//
+//  Created by amin on 2017/5/16.
+//  Copyright © 2017年 yj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShareDetailViewController : UIViewController
+
+@end
